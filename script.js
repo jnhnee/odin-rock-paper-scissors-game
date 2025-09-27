7522ca66def504298b679f989e0d27a24496e504
John Ngueyn <jnhnee@gmail.com>
2025-09-27 18:46:00 -0500

    feat: add js method to return rock,p,s given num

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 const getNum123 = num => (Math.random() * num); // getNum123(3) gives num >= 0 and less than 3
  
+const getComputerChoice = num => { //returns rock,paper,scissors given num
+    if(num > 2) {return "rock"}
+    else if(num > 1) {return "scissors"}
+    else {return "paper"}
+};
+
+console.log(getComputerChoice(getNum123(3)));
+console.log(getComputerChoice(getNum123(3)));
+console.log(getComputerChoice(getNum123(3)));
