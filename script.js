0efe70c876ee3f1adfe376c636f6a70f6c7dd1c2
John Ngueyn <jnhnee@gmail.com>
2025-09-27 19:46:21 -0500

    feat: add playRound js that plays a round with cpu

--- script.js
+++ script.js
@@ -6,6 +6,24 @@ const getComputerChoice = num => { //returns rock,paper,scissors given num
     else {return "paper"}
 };
 
-const getHumanChoice = () => prompt("Enter: rock, paper, scissors").toLowerCase(); //prompts user for input
+let getHumanChoice = () => prompt("Enter: rock, paper, scissors").toLowerCase(); //prompts user for input
+
 
 let humanScore = 0, computerScore = 0;
+
+const playRound = (humanChoice, computerChoice) => {
+    if(humanChoice == "rock" && computerChoice == "scissors") {return (humanScore++, "You Won! Rock beats Scissors")}
+    else if(humanChoice == "scissors" && computerChoice == "paper") {return (humanScore++, "You Won! Scissors beats Paper")}
+    else if(humanChoice == "paper" && computerChoice == "rock") {return (humanScore++, "You Won! Paper beats Rock")}
+    else if(computerChoice == "rock" && humanChoice == "scissors") {return (computerScore++, "You Lose! Rock beats Scissors")}
+    else if(computerChoice == "scissors" && humanChoice == "paper") {return (computerScore++, "You Lose! Scissors beats Paper")}
+    else if(computerChoice == "paper" && humanChoice == "rock") {return (computerScore++, "You Lose! Paper beats Rock")}
+    else {return "Draw! No Score Added"}
+};
+
+console.log(playRound(getHumanChoice(), getComputerChoice(getNum123(3))));
+console.log(`Your Score: ${humanScore}\nComputer Score: ${computerScore}`);
+console.log(playRound(getHumanChoice(), getComputerChoice(getNum123(3))));
+console.log(`Your Score: ${humanScore}\nComputer Score: ${computerScore}`);
+console.log(playRound(getHumanChoice(), getComputerChoice(getNum123(3))));
+console.log(`Your Score: ${humanScore}\nComputer Score: ${computerScore}`);
